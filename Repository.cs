d81d1134717cb992b838916a0c8b713ff6d26230
AbiTravers <abigail.travers@bglgroup.com>
2018-03-23 10:24:41 +0000

    Adding attributes to the repos class so they can be processed correctly

---
+++ Repository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace HttpPracticeConsoleApp
+{
+    [DataContract(Name="repo")]
+    public class Repository
+    {
+        [DataMember(Name = "name")]
+        public string Name
+        {
+            get { return this._name; }
+            set { this._name = value; }
+        }
+
+        private string _name;
+    }
+}
